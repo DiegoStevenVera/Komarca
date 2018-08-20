e3aca0276b215674b44d5c9ad2992ec4904059f4
diegosv10 <diego.vera@unmsm.edu.pe>
2018-08-20 18:13:31 -0500

    Prueba 01

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app" class="Container">
+    <div class="Categories"> 
+      <category v-for="category in categories" :key="category" :name="category.name">
+        
+      </category>
+    </div>
+    <div class="Resume">{{this.message}}</div>
+  </div>
+</template>
+
+<script>
+import Category from './components/Category'
+import {EventBus} from './eventBus.js'
+
+export default {
+  name: 'App',
+  components: {
+    Category
+  },
+  data() {
+    return {
+      message: "Resume",
+      categories: [
+        { name: "Deportes"},
+        { name: "Nacionales"},
+        { name: "Internacionales"},
+        { name: "Tecnología"},
+        { name: "Políticos"},
+        { name: "Espectaculos"}
+      ]
+    }
+  },
+  mounted() {
+    EventBus.$on('estado', msg => {
+      this.message = msg
+    })
+  }
+}
+</script>
+
+<style>
+.Container {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  grid-gap: 10px;
+  padding: 5px;
+  background-color: aqua;
+}
+.Categories {
+  background-color: rgb(0, 216, 144);
+}
+.Resume {
+  grid-column-start: 2;
+  grid-column-end: 5;
+  background-color: rgb(0, 235, 156);
+}
+</style>
---
+++ src/components/Category.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="Category" @click="click()">
+        {{ name }}
+    </div>
+</template>
+
+<script>
+import {EventBus} from './../eventBus.js'
+
+export default {
+  name: 'Category',
+  props: ['name'],
+  methods: {
+      click: function() {
+        EventBus.$emit('estado',this.name)
+      }
+  }
+};
+</script>
+
+<style scoped>
+    .Category {
+        padding: 20px;
+    }
+</style>
